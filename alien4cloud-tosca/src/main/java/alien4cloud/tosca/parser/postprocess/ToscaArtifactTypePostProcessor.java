260771f575e9628861e55ef9c731907bfa509b90
Philippe Benoit <philippe.benoit@fastconnect.fr>
2016-12-22 11:49:33 +0100

    ALIEN-1947: WiP

--- alien4cloud-tosca/src/main/java/alien4cloud/tosca/parser/postprocess/ToscaArtifactTypePostProcessor.java
+++ alien4cloud-tosca/src/main/java/alien4cloud/tosca/parser/postprocess/ToscaArtifactTypePostProcessor.java
@@ -9,6 +9,11 @@ import javax.annotation.Resource;
 import org.alien4cloud.tosca.model.definitions.Operation;
 import org.alien4cloud.tosca.model.types.AbstractInstantiableToscaType;
 import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.nodes.Node;
+
+import alien4cloud.tosca.parser.ParsingContextExecution;
+import alien4cloud.tosca.parser.ParsingError;
+import alien4cloud.tosca.parser.impl.ErrorCode;
 
 /**
  * Performs validation of a type with artifacts.
@@ -22,6 +27,12 @@ public class ToscaArtifactTypePostProcessor implements IPostProcessor<AbstractIn
 
     @Override
     public void process(AbstractInstantiableToscaType instance) {
+        if (safe(instance.getArtifacts()).values().stream().anyMatch(Objects::isNull)) {
+            Node node = ParsingContextExecution.getObjectToNodeMap().get(instance);
+            ParsingContextExecution.getParsingErrors().add(new ParsingError(ErrorCode.INVALID_YAML, "Implementation artifact", node.getStartMark(),
+                    "This node contains an invalid artifact definition", node.getEndMark(), ""));
+            return;
+        }
         safe(instance.getArtifacts()).values().forEach(typeDeploymentArtifactPostProcessor);
         // TODO Manage interfaces inputs to copy them to all operations.
         safe(instance.getInterfaces()).values().stream().flatMap(anInterface -> safe(anInterface.getOperations()).values().stream())
